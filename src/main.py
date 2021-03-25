748b69e0dfb645b6b999a9367c9b7d6ba2ff6e38
markozdemir <32583916+markozdemir@users.noreply.github.com>
2021-03-24 23:47:50 -0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,8 +1,24 @@
-from Pin, time
+import machine, time, dht
 
-led = machine.Pin(2, machine.Pin.OUT)
+# info at https://randomnerdtutorials.com/esp32-esp8266-dht11-dht22-micropython-temperature-humidity-sensor/
+
+sensor = dht.DHT22(machine.Pin(14))
 
 while True:
-  led.value(not led.value())
-  time.sleep(0.5)
+    try:
+        sleep(2)
+        sensor.measure()
+        temp = sensor.temperature()
+        hum = sensor.humidity()
+        print('Temperature: %3.1f C' %temp)
+        print('Humidity: %3.1f %%' %hum)
+    except OSError as e:
+        print('Failed to read sensor.')
 
+
+#import esp32
+## internal temp and hall sensor
+#esp_hs_val = esp32.hall_sensor()
+#esp_temp_val esp32.raw_temperature()
+#print("esp's hall sens val = ", esp_hs_val)
+#print("esp's temp =", esp_temp_val)
