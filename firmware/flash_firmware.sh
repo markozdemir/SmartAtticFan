3f063991095bf86463fefa6f289a112340717fa5
markozdemir <32583916+markozdemir@users.noreply.github.com>
2021-03-24 20:44:54 -0400

    firmware

---
+++ firmware/flash_firmware.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "DELETING FLASH"
+esptool.py --port /dev/tty.usbserial-021FE2F5 erase_flash
+
+echo "FLASHBANG!"
+esptool.py --chip esp32 --port /dev/tty.usbserial-021FE2F5 --baud 460800 write_flash --flash_size=4MB 0 esp32-idf3-20210202-v1.14.bin
