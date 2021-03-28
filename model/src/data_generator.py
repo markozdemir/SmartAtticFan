8c40391cdedc74faf71d9a6514c78adbec3a1536
y-zhang16 <55009104+y-zhang16@users.noreply.github.com>
2021-03-28 15:45:59 -0400

    add data generator

---
+++ model/src/data_generator.py
@@ -0,0 +1,55 @@
+'''
+Data generator for Attic Fan project
+E -- energy consumption
+Phi -- Rotation Speed of Fan
+T -- temperature in the house. (around sensor)
+delta_T -- temperature that makes people feel comfort
+'''
+
+import numpy as np
+import os
+import matplotlib.pyplot as plt
+
+
+def main():
+    ### generate relationship between T and Phi
+    X=np.linspace(1,1000,200,endpoint=True)
+    f_T_phi = np.power(X,-0.2)
+    plt.figure('relationship between T and Phi')
+    plt.title('relationship between T and Phi')
+    plt.xlabel('Phi')
+    plt.ylabel('T')
+
+    plt.plot(f_T_phi)
+    # plt.show()
+    ### import pdb; pdb.set_trace()
+
+    ### generate relationship between E and Phi*t
+    t = np.linspace(1,900,900,endpoint=True)
+    phi = [0,100,200,300,400]
+
+    phi_list = []
+    for each_phi in phi:
+        # import pdb; pdb.set_trace()
+        phi_list.append(t+each_phi)
+
+    phi_arr = np.asarray(phi_list)
+
+
+    plt.figure('relationship between E and Phi*t')
+    plt.title('relationship between E and Phi*t')
+    plt.xlabel('Different phi * t')
+    plt.ylabel('Energy Consumption')
+
+    for each_rela in phi_arr:
+        plt.plot(t, each_rela)
+    plt.show()
+
+    import pdb; pdb.set_trace()
+
+
+    ### define a model that can learn from data to explore targeted relationship
+
+
+if __name__ == '__main__':
+    main()
