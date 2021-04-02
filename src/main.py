8761dca11cea4deb8529aa73288469581769884e
James Mastran <jamesmastran@gmail.com>
2021-04-02 13:49:03 -0400

    Added check method and sending long/lat

--- src/main.py
+++ src/main.py
@@ -1,26 +1,36 @@
-import machine, time, dht, ujson, network, urequests
+import machine, time, dht, ujson, network, urequests, socket
+import binascii
 from ntptime import settime
 
 # info at https://randomnerdtutorials.com/esp32-esp8266-dht11-dht22-micropython-temperature-humidity-sensor/
 aws_URL = "http://ec2-3-141-199-6.us-east-2.compute.amazonaws.com"
 
 sta_if = None
+server_on = True
+connected_wifi = True
+longitude = 0
+latitude = 0
+
 def connect_wifi(ssid, pw):
-    global sta_if
+    global sta_if, connected_wifi
     sta_if = network.WLAN(network.STA_IF)
+
+    fails = 0
     if not sta_if.isconnected():
         sta_if.active(True)
         sta_if.connect(ssid, pw)
         while not sta_if.isconnected():
             time.sleep_ms(200)
-            pass
+            fails += 1
+            if fails > 100:
+                connected_wifi = False
+                return None
     return sta_if.ifconfig()
 
-server_on = True
 def send_data(temp, hum):
-    global server_on
+    global server_on, connected_wifi
 
-    if server_on:
+    if server_on and connected_wifi:
         rpm_val = 0
         power_val = 0
         if temp > 26.7 or hum > 45:
@@ -39,6 +49,39 @@ def send_data(temp, hum):
         r = urequests.request("POST", aws_URL, json=json, headers=headers)
 #        print("response=", r.text)
 
+def format_mac(mac):
+    mac_string = binascii.hexlify(mac)
+    formatted_mac = ""
+    count = 0
+    for c in mac_string:
+        formatted_mac += str(chr(c))
+        if count > 0 and count % 2 == 1:
+            formatted_mac += ":"
+        count += 1
+    print("Formatted mac addr of router from %s to %s" % (mac, formatted_mac))
+    return formatted_mac
+
+def get_location():
+    global connected_wifi, latitude, longitude
+    if not connected_wifi:
+        return (None, None)
+    url = "https://www.googleapis.com/geolocation/v1/geolocate?key="
+    key = "AIzaSyA-mAkb-BG4f0ZIk1mNceujr1fMDVZozRg"
+    mac = format_mac(sta_if.config('mac'))
+    request_j = {
+                  "considerIp": "true",
+                  "wifiAccessPoints": [
+                       {
+                        "macAddress":  mac
+                       }
+                  ]
+                }
+    res = urequests.request("POST", url + key, json=request_j, headers={'content-type': 'application/json'})
+    data = ujson.loads(res.text)
+    location_data = data["location"]
+    latitude = location_data["lat"]
+    longitude = location_data["lng"]
+    return (location_data["lat"], location_data["lng"])
 
 def get_rpm():
     hall_thresh = 100
@@ -70,18 +113,26 @@ def get_rpm():
     
 
 def run():
-    global server_on
-#    print("Trying to connect...")
+    global server_on, connected_wifi
 
-    ipconfig = connect_wifi('ZEYNET', 'Pamukkale1')
+    ipconfig = connect_wifi('ORBI83', 'jaggedzoo924')
+    get_location()
 #    print("ip:", ipconfig[0])
-
-    try:
-        urequests.request("POST", aws_URL, json={"type":"check"}, headers={})
-        print("Server online")
-    except:
-        print("Server offline, this may cause problems")
-        server_on = False
+    if connected_wifi:
+        try:
+            json = { "type":"check",
+                     "data":   { "longitude": longitude,
+                                 "latitude": latitude
+                               }
+                     }
+            r = urequests.request("POST", aws_URL, json=json, headers={})
+            print("Server online: response:", r)
+        except:
+            print("Server offline, this may cause problems")
+            server_on = False
+    else:
+        print("Failed to connect to wifi")
+        sever_on = False
 
     settime()
     sensor = dht.DHT22(machine.Pin(14))
@@ -103,6 +154,7 @@ def run():
 
 
 run()
+
 
 
     
